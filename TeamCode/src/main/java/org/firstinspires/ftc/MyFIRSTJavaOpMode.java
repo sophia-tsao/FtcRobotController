038486d738d92c403825ecc5b926af8b9b1fe845
RagsNiya <rags.niya@gmail.com>
2026-08-14 14:54:34 -0400

    reads left joystick on gamepad 1 and uses that value to control motor power

--- TeamCode/src/main/java/org/firstinspires/ftc/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/MyFIRSTJavaOpMode.java
@@ -1,36 +1,44 @@
-//lists of tasks for robot controller to perform
+package org.firstinspires.ftc;
 
-@TeleOp
-//teleop means driver-controlled. replace with @autonomous to autonomous op mode. 
-    
-public class MyFIRSTJavaOpMode extends LinearOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "My FIRST Java OpMode")
+public class MyFIRSTJavaOpMode extends LinearOpMode{
+
     private Gyroscope imu;
     private DcMotor motorTest;
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange;
     private Servo servoTest;
-    //5 private member variables that hold references to the five configured devices that the onbot java editor that is detected in the config file of your robot controller.
+
 
     @Override
     public void runOpMode() {
-        //overriden method called runOpMode, this must be implemented by every OpMode of type LinearOpMode. gets called whenever a user selects and runs the mode.
+
         imu = hardwareMap.get(Gyroscope.class, "imu");
         motorTest = hardwareMap.get(DcMotor.class, "motorTest");
         digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
         sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
         servoTest = hardwareMap.get(Servo.class, "servoTest");
-        //hardwareMap object is available to use in the runOpMode method. object of the hardwareMap class. 
-        
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        // Wait for the game to start (driver presses PLAY)
+
+        // Wait until PLAY is pressed
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
+        // Keep running until STOP is pressed
         while (opModeIsActive()) {
+
             telemetry.addData("Status", "Running");
             telemetry.update();
-
         }
     }
+
 }
