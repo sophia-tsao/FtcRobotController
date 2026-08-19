2b871750b8e13596b75b49015d67d90060cbafa0
RagsNiya <rags.niya@gmail.com>
2026-08-19 14:35:41 -0400

    Implement drivetrain with four motors and control logic
    Refactored motor control logic and added multiple motors for drivetrain.

--- TeamCode/src/main/java/org/firstinspires/ftc/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/MyFIRSTJavaOpMode.java
@@ -12,20 +12,39 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class MyFIRSTJavaOpMode extends LinearOpMode{
 
     private Gyroscope imu;
-    private DcMotor motorTest;
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange;
     private Servo servoTest;
+
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
 
 
     @Override
     public void runOpMode() {
 
+        // get devices from robot configuration
         imu = hardwareMap.get(Gyroscope.class, "imu");
-        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
         digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
         sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
         servoTest = hardwareMap.get(Servo.class, "servoTest");
+        //initializing front left right back left right motors
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        //because motors mounted on opposite sides physically face opposite
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        // make motors resist movement when power is 0
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -34,15 +53,49 @@ public class MyFIRSTJavaOpMode extends LinearOpMode{
         waitForStart();
 
         //run until stop is pressed
-        double tgtPower = 0;
-
         while (opModeIsActive()){
-            //reads vertical position of left joystick
-            tgtPower = -this.gamepad1.left_stick_y; 
-            // y=-1 joystick in topmost position and y=+1 when joystick in botommost position
-            motorTest.setPower(tgtPower);
-            telemetry.addData("Target Power", tgtPower);
-            telemetry.addData("Motor power", motorTest.getPower());
+
+            //left stick controls backwards/forwards
+            double drive = -gamepad1.left_stick_y;
+
+            //right stick controls turning
+            double turn = gamepad1.right_stick_x;
+
+            double leftPower = drive + turn;
+            double rightPower = drive - turn;
+
+            //slow mode by holding left bumper
+            double speedMultiplier = 1.0;
+            if (gamepad1.left_bumper){
+                speedMultiplier = 0.4;
+            }
+            leftPower *= speedMultiplier;
+            rightPower *= speedMultiplier;
+
+            //prevent calculated motor powers from going above 1 or below -1
+            double max = Math.max(
+                    Math.abs(leftPower),
+                    Math.abs(rightPower)
+            );
+
+            if (max>1.0){
+                leftPower /= max;
+                rightPower /= max;
+            }
+
+            //send calculated power to drivetrain motors
+            frontLeft.setPower(leftPower);
+            backLeft.setPower(leftPower);
+
+            frontRight.setPower(rightPower);
+            backRight.setPower(rightPower);
+
+            //telemetry
+            telemetry.addData("Drive", drive);
+            telemetry.addData("Turn", turn);
+            telemetry.addData("Left Power", leftPower);
+            telemetry.addData("Right Power", rightPower);
+            telemetry.addData("Slow Mode", gamepad1.left_bumper);
             telemetry.addData("Status", "Running");
             telemetry.update();
 
